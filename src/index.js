f1f877058d13083ea7066db20debfbc37d9b2df8
jpxyzog <119944675+jpxyzog@users.noreply.github.com>
2023-03-02 20:05:02 -0500

    lg

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Dashboard from './components/Dashboard';
-
+import { BrowserRouter as Router } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <Navbar />
-    <Dashboard />
-    <Home />
-    <Stocks />
-    <About />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
